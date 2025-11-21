e064379e974b2a3cd392dba3fd7d8588ec95f14b
jeremiahbwala <charlesjeremiah204@gmail.com>
2025-11-21 11:34:12 +0100

    Enhance Hero component with sub-heading and button
    Added a sub-heading and a button section to the Hero component.

--- Documents/devcheque--/src/Hero.jsx
+++ Documents/devcheque--/src/Hero.jsx
@@ -12,6 +12,17 @@ function Hero() {
           <p>Desig. Develop. Deploy.
           Get Your Product Cut Launched in Week.</p>
           </div>
+          <div className="sub-heading">
+              <p>Devcheque is your full-stack
+              team for digital products that ship.
+              From branding to  UI/UX and development,
+              we handle it all. 
+              Launch fasterwith top quality.
+              </p>
+          </div>
+          <div className="btn">
+              <img src="" alt="hero image" />
+          </div>
       </div>
     </>
   )
